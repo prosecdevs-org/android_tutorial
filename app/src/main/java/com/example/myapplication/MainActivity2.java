ca09287e4aa9aa42e446412f5324d1bed2d75344
prince <priyanshusingh.rajput@rediffmail.com>
2023-10-03 22:32:31 +0530

    Linear,Grid Layout & Alert Dialog

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,10 +1,18 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,13 +22,42 @@ public class MainActivity2 extends AppCompatActivity {
     String text;
     MyAdapter adapter;
     RecyclerView recyclerView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         List<String> list = Arrays.asList("Str1","Str2");
+        button = findViewById(R.id.btn2);
         recyclerView = findViewById(R.id.recyclerFirst);
         adapter = new MyAdapter(getApplicationContext(),list);
+        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
         recyclerView.setAdapter(adapter);
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this);
+        builder.setTitle("Hello");
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.new_view,null);
+        TextView t1 = view.findViewById(R.id.txt3);
+        t1.setText("Hello Priyanshu");
+        builder.setView(view);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+
     }
 }
