012df5fcebb06ecdab80ee5a6e569e5f0ddec6ac
prince <priyanshusingh.rajput@rediffmail.com>
2023-10-25 00:34:50 +0530

    Date Picker in android

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,34 +3,54 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
     Button button;
+    TextView txt1;
 
+    Calendar mCalendar = Calendar.getInstance();
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt1 = findViewById(R.id.txt1);
         button = findViewById(R.id.btn1);
         editText = findViewById(R.id.edit1);
+
+        int year = mCalendar.get(Calendar.YEAR);
+        int month = mCalendar.get(Calendar.MONTH);
+        int dayOfMonth = mCalendar.get(Calendar.DAY_OF_MONTH);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
-                String text = editText.getText().toString();
-                intent.putExtra("key",text);
-                startActivity(intent);
-                Toast.makeText(MainActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+//                String text = editText.getText().toString();
+//                intent.putExtra("key",text);
+//                startActivity(intent);
+//                Toast.makeText(MainActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        txt1.setText(dayOfMonth+"-"+month+"-"+year);
+                    }
+                },year,month,dayOfMonth);
+                datePickerDialog.show();
+                //Time Picker Self Implement
             }
         });
 
